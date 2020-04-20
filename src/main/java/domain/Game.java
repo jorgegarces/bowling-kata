dd61ab1674bc9fa9517addde42203dda256947dd
Jorge <dam0ne@hemwick>
2020-04-20 08:57:50 +0200

    test: game to score 1 on a 1 point throw

---
+++ src/main/java/domain/Game.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Game {
+    public void roll(int i) {
+
+    }
+
+    public Score score() {
+        return null;
+    }
+}
---
+++ src/test/java/GameShould.java
@@ -0,0 +1,16 @@
+import domain.Game;
+import domain.Score;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GameShould {
+    @Test
+    public void score_1_on_a_1_point_throw() {
+
+        Game game = new Game();
+        Score score = new Score(1);
+        game.roll(1);
+
+        Assert.assertEquals(score, game.score());
+    }
+}
