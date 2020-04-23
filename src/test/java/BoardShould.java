e2f0cffe8ac56db6292816a1e56242b4604a04bd
Jorge <dam0ne@hemwick>
2020-04-23 09:57:37 +0200

    feat: board calculates scores for spare and strike bonuses

--- src/test/java/BoardShould.java
+++ src/test/java/BoardShould.java
@@ -48,4 +48,50 @@ public class BoardShould {
 
         Assert.assertEquals(testScore, board.getScore());
     }
+
+    @Test
+    public void sum_scores_of_frames_with_spare_bonus(){
+
+        Board board = new Board();
+        Score testScore = new Score();
+        testScore.update(12);
+
+        board.addRoll(8);
+        board.addRoll(2);
+        board.addRoll(1);
+
+        Assert.assertEquals(testScore, board.getScore());
+    }
+    @Test
+    public void sum_scores_of_frames_with_2_consecutive_spare_bonuses(){
+
+        Board board = new Board();
+        Score testScore = new Score();
+        testScore.update(36);
+
+        board.addRoll(8);
+        board.addRoll(2);
+        board.addRoll(2);
+        board.addRoll(8);
+        board.addRoll(5);
+        board.addRoll(4);
+
+        Assert.assertEquals(testScore, board.getScore());
+    }
+    @Test
+    public void sum_scores_of_frames_combining_strike_and_spare_bonuses(){
+
+        Board board = new Board();
+        Score testScore = new Score();
+        testScore.update(55);
+
+        board.addRoll(10);
+        board.addRoll(10);
+        board.addRoll(2);
+        board.addRoll(8);
+        board.addRoll(1);
+        board.addRoll(1);
+
+        Assert.assertEquals(testScore, board.getScore());
+    }
 }
